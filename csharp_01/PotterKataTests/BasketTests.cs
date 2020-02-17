231acaae09fb321aec90b17eee0f2593bab60ff5
Jon Cain <jonifen@users.noreply.github.com>
2020-02-17 22:03:11 +0000

    CS1 - Fourth and fifth tests

--- csharp_01/PotterKataTests/BasketTests.cs
+++ csharp_01/PotterKataTests/BasketTests.cs
@@ -14,9 +14,11 @@ namespace PotterKataTests
         }
 
         [Test]
-        [TestCase(new int[] { 1 }, 8, TestName = "Scan One Book, Get Price")]
-        [TestCase(new int[] { 1, 2 }, 15.20, TestName = "Scan Two Different Books, Get Price")]
-        [TestCase(new int[] { 1, 2, 3 }, 21.60, TestName = "Scan Three Different Books, Get Price")]
+        [TestCase(new int[] { 1 }, 8.00, TestName = "1) Scan One Book, Get Price")]
+        [TestCase(new int[] { 1, 2 }, 15.20, TestName = "2) Scan Two Different Books, Get Price")]
+        [TestCase(new int[] { 1, 2, 3 }, 21.60, TestName = "3) Scan Three Different Books, Get Price")]
+        [TestCase(new int[] { 1, 2, 3, 4 }, 25.60, TestName = "4) Scan Four Different Books, Get Price")]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 30.00, TestName = "5) Scan Five Different Books, Get Price")]
         public void BasketTestCases(int[] books, decimal expectedPrice)
         {
             _basket.Add(books);
