42f1d1cc250024a26a166e615bed3c2d281c9502
Jon Cain <jonifen@users.noreply.github.com>
2020-11-29 13:53:25 +0000

    Repo restructure, moved attempts into own branches

---
+++ PotterKata/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace PotterKata
+{
+    public class Book
+    {
+        public Book(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+
+        public decimal Price {
+            get
+            {
+                return 8.00m;
+            }
+        }
+    }
+}
